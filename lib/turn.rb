8d80159b32536c19fb9350a67165ac5a80f9b32d
Tom Banish <tombanish@gmail.com>
2019-11-18 01:11:33 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -45,7 +45,7 @@ def turn(board)
   user_input = gets.strip
   input_to_index(user_input)
   valid_move?(board, index)
-  if valid_move?(board, index) == true
+  if valid_move?(board, index)
     move(board, user_input, char="X")
 end
 
