091fb28427a92cabbabc415df5f01c7368a7fa4f
Tom Banish <tombanish@gmail.com>
2019-11-18 02:05:10 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,58 @@
+# display_board method
+board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# valid_move? method
+def valid_move?(board, index)
+  if position_taken?(board, index) == false && index.between?(0,8) == true
+    true
+  else position_taken?(board, index) == true
+    false
+  end
+end
+
+# postion_taken? method
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index]== nil
+    false
+  else board[index] == "X" || "O"
+    true
+  end
+end
+
+
+# move method
+user_input = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, user_input, char="X")
+  board[user_input] = char
+end
+
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  user_input = input_to_index(user_input)
+  if valid_move?(board, user_input)
+    move(board, user_input, char="X")
+    display_board(board)
+  else until valid_move?(board, user_input) == true
+      turn(board)
+      break
+    end
+  end
+end
+
+
+
